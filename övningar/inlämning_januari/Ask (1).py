6ad72217e94d329921054e46d2a47b8c5506529d
oskarahlqwist <oskarahlqwist@gmail.com>
2021-01-11 20:07:10 +0100

    master

---
+++ övningar/inlämning_januari/Ask (1).py
@@ -0,0 +1,35 @@
+
+
+
+## input till funktionen är (data, datatype, message)
+
+# data = input från användare
+# datatype = vad vi skall kontrollerar "Int","Float", "String"
+# message = Felmeddelande
+
+# Retur -- boolean True/False
+# Retur -- data omvandlad till rätt datatype
+def check_datatype(data,datatype,message):
+    # if sats som kollar datatype
+    # if string
+        # kontrollera om data uppfyller kraven för att vara en string
+         # retur True, data
+         # - retur False
+         #  - print(message) 
+    # elif int
+        # kontrollera om data uppfyller kraven för att vara en int
+         # retur True int(data)
+         # - retur False
+         #  - print(message)
+    # elif float
+        # kontrollera om data uppfyller kraven för att vara en float
+         # retur True, float(data)
+         # - retur False
+         #  - print(message)
+    # else Något annat är fel
+        # print "något annat är fel"
+
+
+
+
+def check_interval():
