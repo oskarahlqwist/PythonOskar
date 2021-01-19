c27b72d94519192128b237f13c05e422771db440
oskarahlqwist <oskarahlqwist@gmail.com>
2021-01-19 21:44:30 +0100

    master

--- övningar/strängar/strängövn-lösningar/dict_2.py
+++
@@ -1,9 +0,0 @@
-t = (1, 1.25, 1.75, "oskar")
-
-summan = 0
-
-for nummer in t:
-    if isinstance(nummer, int) or isinstance(nummer, float):
-        summan += nummer
-
-print ("Summan av nummerna i  tuplen är: ", summan)
--- övningar/strängar/strängövn-lösningar/string3.py
+++
@@ -1,3 +0,0 @@
-lite_text = 'Hejsan hoppsan i lingonskogen'
-ord_lista = lite_text.split()
-print(ord_lista)
