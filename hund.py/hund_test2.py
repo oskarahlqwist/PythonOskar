1eabd9e7840a40ae7d3c6f2547d8fbf476bcd2f0
oskarahlqwist <oskarahlqwist@gmail.com>
2021-02-19 14:52:01 +0100

    update

---
+++ hund.py/hund_test2.py
@@ -0,0 +1,15 @@
+from hund2 import Hund
+##from hundagare import Hundagare
+
+h = Hund("Fido","Poodle","familjehund",2,"svart")
+print (h)
+
+h = Hund("Spot","Dauschhund","korkad",3,"rödbrun")
+print (h)
+
+h.fodelsedag()
+print (h)
+
+##a = Hundagare("Charlie","123 Hundgatan",345,"Ordförande")
+##h.set_agare(a)
+##print (h)
--- testa.py
+++ testa.py
@@ -1,9 +1,11 @@
-def fibonacci(n):
-    if n==1:
-        return 1
-    elif n==2:
-        return 1
-    elif n>2:
-        return fibonacci(n-1) + fibonacci(n-2)
-for n in range (1, 11):
-    print(n, ":", fibonacci(n))
+from dataclasses import dataclass
+
+@dataclass
+class Position:
+    name: str
+    lon: float
+    lat: float
+
+t = Position("Oskar", 20.32, 23.55)
+
+print(f'{t.name} is at {t.lat}°N, {t.lon}°E')
