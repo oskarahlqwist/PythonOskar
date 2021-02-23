c522f3cd6afe3f7afb04eeb4dbb7a35b9fc340a8
oskarahlqwist <oskarahlqwist@gmail.com>
2021-02-23 18:12:58 +0100

    update

--- testa.py
+++ testa.py
@@ -1,5 +1,6 @@
-import numpy as np
+import matploblib.pyplot as plt
 
-arr = np.array (42)
+x = list(range(10))y = [3, 6, 39, 48, 54, 98, 130, 145, 143, 167]
 
-print(arr)
+plt.plot(x,y)
+plt.show()
