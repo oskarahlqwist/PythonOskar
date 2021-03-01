d5ee04cdf217e6396240d95103b007fedb2c7e4a
oskarahlqwist <oskarahlqwist@gmail.com>
2021-03-01 21:18:12 +0100

    u

--- testa.py
+++ testa.py
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
-import matplotlib.pyplot as plt
-import numpy as np
+import sqlite3
+conn = sqlite3.connect('sp.db')
+cursor = conn.cursor()
 
-#plot 1:
-x = np.array([0, 1, 2, 3])
-y = np.array([3, 8, 1, 10])
+sno = input("För vilken leverantör vill du se antal levererade?: ")
 
-plt.subplot(2, 1, 1)
-plt.plot(x,y)
-
-#plot 2:
-x = np.array([0, 1, 2, 3])
-y = np.array([10, 20, 30, 40])
-
-plt.subplot(2, 1, 2)
-plt.plot(x,y)
-
-=======
-import matplotlib.pyplot as plt 
-import pandas as pd
-
-df = pd.read_csv('data_by_genres.csv', sep = ',', encoding='utf-8')
-df.plot(kind = 'scatter', x = 'tempo', y = 'energy', alpha = a)
->>>>>>> fa5061e14f49a283fd8984bfa447b3e8e7de17d5
-plt.show()
+cursor.execute('''Select sum(qty) from sp where sno = &sno ''')
+all_rows = cursor.fetchall()
+print('-------------------------------------')
+for row in all_rows:
+    print (row[0])
+print()
+conn.close()
