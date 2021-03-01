b3dde611d76c0002ac248ec446dff8922c4e16bd
oskarahlqwist <oskarahlqwist@gmail.com>
2021-03-01 18:35:04 +0100

    u

--- testa.py
+++ testa.py
@@ -1,6 +1,18 @@
-import matploblib.pyplot as plt
+import matplotlib.pyplot as plt
+import numpy as np
 
-x = list(range(10))y = [3, 6, 39, 48, 54, 98, 130, 145, 143, 167]
+#plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
 
+plt.subplot(2, 1, 1)
 plt.plot(x,y)
+
+#plot 2:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 1, 2)
+plt.plot(x,y)
+
 plt.show()
