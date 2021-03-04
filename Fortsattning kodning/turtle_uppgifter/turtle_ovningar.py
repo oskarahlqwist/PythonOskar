b987a0087b14369c3536ff4aadaf192afce64de0
oskarahlqwist <oskarahlqwist@gmail.com>
2021-03-04 13:49:22 +0100

    u

--- Fortsattning kodning/turtle_uppgifter/turtle_ovningar.py
+++ Fortsattning kodning/turtle_uppgifter/turtle_ovningar.py
@@ -158,3 +158,25 @@ t.forward (15)
 t.end_fill()
 
 turtle.done()
+
+##5
+import turtle
+
+t = turtle.Turtle()
+t.speed(1000)
+
+for i in range(3):
+  t.forward(200)
+  t.left(120)
+
+t.forward(95)
+
+for i in range(36):
+  t.forward(10)
+  t.left(10)
+
+t.forward(6)
+t.left(90)
+t.forward(172)
+
+turtle.done()
--- test.py
+++ test.py
@@ -1,85 +1,20 @@
-# Import required module 
-import turtle 
+import turtle
 
-# Create turtle object 
-t = turtle.Turtle() 
+t = turtle.Turtle()
+t.speed(1000)
 
-# Create a screen 
-screen =turtle.Screen() 
+for i in range(3):
+  t.forward(200)
+  t.left(120)
 
-# Set background color 
-screen.bgcolor("sky blue") 
+t.forward(95)
 
-# Function to draw body of snowman 
-def draw_circle(color, radius, x, y): 
-	t.penup() 
-	t.fillcolor (color) 
-	t.goto (x, y) 
-	t.pendown() 
-	t.begin_fill() 
-	t.circle (radius) 
-	t.end_fill() 
+for i in range(36):
+  t.forward(10)
+  t.left(10)
 
-# Illustrating snowman 
-# Drawing snowman body 
-draw_circle ("#ffffff", 30, 0, -40) 
-draw_circle ("#ffffff", 40, 0, -100) 
-draw_circle ("#ffffff", 60, 0, -200) 
-
-# Drawing left eye 
-draw_circle ("#ffffff", 2, -10, -10) 
-
-# Drawing right eye 
-draw_circle ("#ffffff", 2, 10, -10) 
-
-# Drawing nose 
-draw_circle ("#FF6600", 3, 0, -15) 
-
-# Drawing buttons on 
-draw_circle ("#ffffff", 2, 0, -35) 
-draw_circle ("#ffffff", 2, 0, -45) 
-draw_circle ("#ffffff", 2, 0, -55) 
-
-# Function to draw arms 
-def create_line(x, y, length, angle): 
-	t.penup() 
-	t.goto(x, y) 
-	t.setheading(angle) 
-	t.pendown() 
-	t.forward(length) 
-	t.setheading(angle + 20) 
-	t.forward(20) 
-	t.penup() 
-	t.back(20) 
-	t.pendown() 
-	t.setheading(angle - 20) 
-	t.forward(20) 
-	t.penup() 
-	t.home() 
-	
-# Drawing left arm 
-create_line(-70, -50, 50, 160) 
-
-# Drawing right arm 
-create_line(70, -50, 50, 20) 
-
-# Drawing hat 
-t.penup() 
-t.goto (-35, 8) 
-t.color ("black") 
-t.pensize (6) 
-t.pendown() 
-t.goto (35, 8) 
-
-t.goto (30, 8) 
-t.fillcolor ("black") 
-t.begin_fill() 
-t.left (90) 
-t.forward (15) 
-t.left (90) 
-t.forward (60) 
-t.left (90) 
-t.forward (15) 
-t.end_fill()
+t.forward(6)
+t.left(90)
+t.forward(172)
 
 turtle.done()
