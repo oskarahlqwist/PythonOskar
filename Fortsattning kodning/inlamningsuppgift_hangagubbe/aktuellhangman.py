a0ac3ea208a93e4d8bbe0f0f8683615e86046b5e
oskarahlqwist <oskarahlqwist@gmail.com>
2021-03-17 17:28:28 +0100

    u

--- Fortsattning kodning/inlamningsuppgift_hangagubbe/aktuellhangman.py
+++ Fortsattning kodning/inlamningsuppgift_hangagubbe/aktuellhangman.py
@@ -24,29 +24,27 @@ t.forward(25)
 t.penup()
 t.right(90)
 t.pendown()
-
 t.color('yellow')
 t.speed(5)
 
-the_word = random.choice(open('ordlista.txt').read().split())
+ordet = random.choice(open('D:\Kodning\Python projekt\Fortsattning kodning\inlamningsuppgift_hangagubbe\ordlista.txt').read().split())
 
-#Struktur för turtle
-HANGMAN = ['1','2','3','4','5','6']
+#Hur många försök man får
+antal_liv = ['1','2','3','4','5','6']
 
 class Hangagubbe():
-  
-    def __init__(self, the_word):
+    def __init__(self, ordet):
         self.gissningar = 0
-        self.the_word = the_word
-        self.game_play = list('_' * len(self.the_word))
+        self.ordet = ordet
+        self.ord_status = list('_' * len(self.ordet))
         self.gissade_bokstaver = []
 
     def indexes(self, letter):
     ##Räknar hur många bostäver det slumpmässiga ordet har
     ##tar in en bokstav och retunerar med indexet som den har i ordet
     ##går ingenom varje bokstav i ordet retunerar index för varje bokstav
-    ##enurmarete gör varje bokstav i "the_word" med till hörande index till ett "object" "tupel"
-        return [i for i, char in enumerate(self.the_word) if letter == char]
+    ##enurmarete gör varje bokstav i "ordet" med till hörande index till ett "object" "tupel"
+        return [i for i, char in enumerate(self.ordet) if letter == char]
 
     def check_input(self, input_): 
     ##Kontrollerar att gissningen är EN bokstav
@@ -54,9 +52,10 @@ class Hangagubbe():
     ## kollar om bokstaven är en siffra eller längre än 2 tecken  
         return (input_.isdigit() or len(input_) > 1) 
 
-    def print_game_status(self):
-    ##Lägger till mellanrum mellan _ i terminalen samt kallar på game_play"""
-        print(' '.join(self.game_play))
+    def print_ord_status(self):
+    ##Lägger till mellanrum mellan _ i terminalen samt kallar på ord_status
+    ## printar bokstäver som du gissat rätt och sätter " _ " på där bokstäver saknas
+        print(' '.join(self.ord_status))
     
     def check_gissade_bokstaver(self, letter):
     ##Lägger till bokstaven i en lista med använda bokstäver och printar ut
@@ -66,39 +65,39 @@ class Hangagubbe():
             self.gissade_bokstaver.append(letter)
             print("\n""Använda bokstäver: ", " ".join(self.gissade_bokstaver))
 
-    def update_progress(self, letter, indexes):
-    ##Tar in indexet i ordet som bokstaven ligger i och lägger in den bokstaven på rätt index i game_play.
+    def update_ord_status(self, letter, indexes):
+    ##Tar in indexet i ordet som bokstaven ligger i och lägger in den bokstaven på rätt index i ord_status.
         for index in indexes:
-            self.game_play[index] = letter
+            self.ord_status[index] = letter
 
-    def get_user_input(self):
+    def get_gissad_bokstav(self):
     ##Tar in user input och kontrollerar att det är en bokstav samt hanterar ifall det är versal/icke-versal
-            user_input = input('\nGissa en bokstav: ')
-            if user_input.isalpha():
-                user_input = user_input.upper()
-                return user_input
+            gissad_bokstav = input('\nGissa en bokstav: ')
+            if gissad_bokstav.isalpha():
+                gissad_bokstav = gissad_bokstav.upper()
+                return gissad_bokstav
             else:
-                return(user_input)
+                return(gissad_bokstav)
         
-    def play(self):
-        #Startar spelet genom att fråga om en första bokstav och fortsätter tills spelaren inte har några försök kvar
-        while self.gissningar < len(HANGMAN):
-            self.print_game_status()
-            user_input = self.get_user_input()
+    def spela(self):
+        #Startar spelet genom att fråga om en första bokstav och fortsätter tills ord_statusren inte har några försök kvar
+        while self.gissningar < len(antal_liv):
+            self.print_ord_status()
+            gissad_bokstav = self.get_gissad_bokstav()
             #Lägger till bokstaven i en lista
-            self.check_gissade_bokstaver(user_input)
+            self.check_gissade_bokstaver(gissad_bokstav)
             
             # Kontrollerar ifall bokstaven redan gissats
-            if user_input in self.game_play:
+            if gissad_bokstav in self.ord_status:
                 print('Du har redan gissat på den bokstaven')
                 continue
             ##Tar ut vilken plats/index i ordet bokstaven ligger på.
-            if user_input in self.the_word:
-                indexes = self.indexes(user_input)
-                self.update_progress(user_input, indexes)
+            if gissad_bokstav in self.ordet:
+                indexes = self.indexes(gissad_bokstav)
+                self.update_ord_status(gissad_bokstav, indexes)
                 
-                if self.game_play.count('_') == 0:
-                    print('\n DU VANN!')
+                if self.ord_status.count('_') == 0:
+                    print('\nDU VANN!')
                     t.speed('slow')
                     t.color('black')
                     style = ('Arial', 30, 'italic')
@@ -106,8 +105,9 @@ class Hangagubbe():
                     t.goto(0,-50)
                     t.pendown()
                     t.write('DU VANN :D ', font=style, align='center')
-                    print('Ordet var: {0}'.format(self.the_word))
-                    quit()
+                    print('Ordet var: {0}'.format(self.ordet))
+                    turtle.done()
+                    
             else:
                 self.gissningar += 1
                 if self.gissningar==1:
@@ -145,7 +145,7 @@ class Hangagubbe():
                     t.left(120)
                     t.forward(45)
 
-        print("\n DU FÖRLORA")
+        print("\nDU FÖRLORA")
         t.speed('slow')
         t.color('black')
         style = ('Arial', 30, 'italic')
@@ -153,10 +153,9 @@ class Hangagubbe():
         t.goto(0,-50)
         t.pendown()
         t.write('LOOSER!', font=style, align='center')
+        print('Ordet var: {0}'.format(self.ordet))
         turtle.done()
-        print('Ordet var: {0}'.format(self.the_word))
-        quit()
 
-the_word = the_word.upper()
-Hangagubbe = Hangagubbe(the_word)
-Hangagubbe.play()
+ordet = ordet.upper()
+Hangagubbe = Hangagubbe(ordet)
+Hangagubbe.spela()
